95a32a97c46e3882c250a5faa3ceba05003f4e43
Jair Sousa <jairsmarques1126@gmail.com>
2022-09-15 11:57:38 -0300

    Criando o primeiro componente

--- curso-angular/fundamentos-angular/src/app/app.module.ts
+++ curso-angular/fundamentos-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroCmponenteComponent } from './primeiro-componente/primeiro-componente.componente';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroCmponenteComponent
   ],
   imports: [
     BrowserModule,
