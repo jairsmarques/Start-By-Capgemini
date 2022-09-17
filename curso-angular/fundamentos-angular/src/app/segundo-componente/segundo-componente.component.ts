c650aed5e12c72475e05f9391885a3d0a41a3459
Jair Sousa <jairsmarques1126@gmail.com>
2022-09-17 10:58:05 -0300

    Trabalahando com pipes, botões e estilos

--- curso-angular/fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
+++ curso-angular/fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
@@ -7,4 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SegundoComponenteComponent {
   name = "João";
+  dataNascimento = "1995-01-01";
+  urlImagem = "/assets/joao.jpg";
+
+  mostrarDataNascimento() {
+    alert(`A data de nascimento do João é: ${this.dataNascimento}`)
+  }
 }
